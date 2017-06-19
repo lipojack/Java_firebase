65d030e0d91a4e25ea1fad69c8e0ebc275640e2b
lipojack <cp000123@gmail.com>
2017-06-19 12:30:01 +0800

    emotion added

--- src/socketsever/SocketSever.java
+++ src/socketsever/SocketSever.java
@@ -21,21 +21,16 @@ import com.google.firebase.FirebaseOptions;
 import com.google.firebase.auth.FirebaseCredentials;
 import com.google.firebase.database.*;
 
-
-
 public class SocketSever {
 
-	//private static Thread th_close;
-	//private static int serverport = 7777;
-	//private static ServerSocket serverSocket;
-	//private static ArrayList<Socket> socketlist = new ArrayList<Socket>();
-	//private static Map gazeMap = new HashMap();
-	//private static Map gestureMap = new HashMap();
 
+	// Provide absolute path
 	//private static String gazeFile = "W:\\GazeEstimationFor志軒\\IntraFace-v1.2\\x64\\Release\\output_gaze.json";
 	//private static String gestureFile = "W:\\demo\\HandPoseRecognition-master\\HandPoseRecognition\\Release\\output_gesture.json";
 	//private static String emotionFile = "";
 
+
+	// Test file
 	private static String gestureFile = "output_gesture.json";
 	private static String gazeFile = "output_gaze.json";
 	private static String emotionFile = "output_face.json";
@@ -54,7 +49,7 @@ public class SocketSever {
 	static int value_gaze;
 	static int value_emotion;
 
-	static int value_gaze_pre = -1;
+	//static int value_gaze_pre = -1;
 
 
 	public static void main(String[] args) {
@@ -67,7 +62,7 @@ public class SocketSever {
 					.setDatabaseUrl("https://nthusmarttv-d20c4.firebaseio.com/")
 					.build();
 			FirebaseApp.initializeApp(options);
-			System.out.println("success");
+			System.out.println("Firebase connect success");
 			// [END initialize]
 		} catch (IOException e) {
 			System.out.println("ERROR: invalid service account credentials. See README.");
@@ -77,25 +72,17 @@ public class SocketSever {
 
 		// Create database reference
 		final FirebaseDatabase database = FirebaseDatabase.getInstance();
-		DatabaseReference ref = database.getReference();
-		DatabaseReference gestureRef = ref.child("gesture");
-		DatabaseReference gazeRef = ref.child("gaze");
-		DatabaseReference emotionRef = ref.child("emotion");
+		DatabaseReference ref = database.getReference("User/103087087");
+		DatabaseReference gestureRef = ref.child("Gesture").child("gesture");
+		DatabaseReference gazeRef = ref.child("Gaze").child("gaze");
+		DatabaseReference emotionRef = ref.child("Expression").child("expr");
 
 		try {
-			//gestureMap.clear();
-			// gestureFile = args[0];
-			// gazeFile = args[1];
 
 			System.out.println("Start");
 
-			// set sever port
-			//serverSocket = new ServerSocket(serverport);
-			//th_close = new Thread(Judge_Close);
-			//th_close.start();
-			//while (!serverSocket.isClosed()) {
-				//waitNewSocket();
-			//}
+			//Filtering json data
+			//Uploading json data to Firebase
 			while (true) {
 				// Gesture
 				String filename = gestureFile;
@@ -148,7 +135,6 @@ public class SocketSever {
 				gazesb.append(line);
 				string = gazesb.toString();
 				JSONObject gazeJson = new JSONObject(string);
-
 				// filter: get mode
 				buffer_gaze[counter_gesture % 10] = gazeJson.getInt("value");
 				counter_gaze++;
@@ -176,7 +162,14 @@ public class SocketSever {
 					*/
 				}
 				gazebr.close();
-
+				// Emotion
+				// 0:'Anger'
+				// 1:'Disgust'
+				// 2:'Fear'
+				// 3:'Happiness'
+				// 4:'Sadness'
+				// 5:'Surprise'
+				// 6:'Neutral'
 				filename = emotionFile;
 				BufferedReader emotionbr = new BufferedReader(new FileReader(filename));
 				StringBuilder emotionsb = new StringBuilder();
@@ -193,8 +186,33 @@ public class SocketSever {
 				if (counter_emotion > 9) {
 					value_emotion = mode(buffer_emotion);
 					System.out.println("Emotion : " + value_emotion);
+					switch(value_emotion){
+						case 0:
+							emotionRef.setValue("Anger");
+							break;
+						case 1:
+							emotionRef.setValue("Disgust");
+							break;
+						case 2:
+							emotionRef.setValue("Fear");
+							break;
+						case 3:
+							emotionRef.setValue("Happiness");
+							break;
+						case 4:
+							emotionRef.setValue("Sadness");
+							break;
+						case 5:
+							emotionRef.setValue("Suprise");
+							break;
+						case 6:
+							emotionRef.setValue("Neutral");
+							break;
+						default:
+							System.out.println("Emotion exception");
+							break;
+					}
 
-					emotionRef.setValue(value_emotion);
 				}
 				emotionbr.close();
 
@@ -204,56 +222,6 @@ public class SocketSever {
 			e.printStackTrace();
 		}
 	}
-
-/*
-	// detect disconnection
-	private static Runnable Judge_Close = new Runnable() {
-		@Override
-		public void run() {
-			System.out.println("Judge_Close run()");
-			try {
-				while (true) {
-					// run every 2 sec
-					Thread.sleep(2000);
-					for (Socket close : socketlist) {
-						if (isServerClose(close)) {
-							// remove socket from socket list when disconnect
-							socketlist.remove(close);
-							System.out.println("socketlist.remove");
-						}
-					}
-				}
-			} catch (Exception e) {
-				e.printStackTrace();
-			}
-		}
-	};
-
-	// detect disconnection
-	private static Boolean isServerClose(Socket socket) {
-		System.out.println("isServerClose");
-		try {
-			// if connect
-			socket.sendUrgentData(0);
-			return false;
-		} catch (Exception e) {
-			// if disconnect
-			return true;
-		}
-	}
-*/
-/*
-	// wait for client connection
-	public static void waitNewSocket() {
-		System.out.println("waitNewSocket");
-		try {
-			Socket socket = serverSocket.accept();
-			createNewThread(socket);
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-	}
-*/
 
 	// return the mode of the 10 numbers
 	public static int mode(int[] array) {
@@ -275,105 +243,5 @@ public class SocketSever {
 		}
 		return temp;
 	}
-
-
-	// create thread
-	/*public static void createNewThread(final Socket socket) {
-		System.out.println("createNewThread");
-		Thread t = new Thread(new Runnable() {
-			@Override
-			public void run() {
-				try {
-					socketlist.add(socket);
-
-					BufferedOutputStream out = new BufferedOutputStream(socket.getOutputStream());
-
-					while (socket.isConnected()) {
-						
-						// Gesture
-						// read file
-						String filename = gestureFile;
-						BufferedReader gesturebr = new BufferedReader(new FileReader(filename));
-						StringBuilder gesturesb = new StringBuilder();
-						String line = gesturebr.readLine();
-						while (line == null) {
-							line = gesturebr.readLine();
-						}
-						gesturesb.append(line);
-						String string = gesturesb.toString();
-						JSONObject gestureJson = new JSONObject(string);
-						
-						// filter: get mode
-						buffer_gesture[counter_gesture % 10] = gestureJson.getInt("value");
-						counter_gesture++;
-						if (counter_gesture > 9) {
-							value_gesture = mode(buffer_gesture);
-							System.out.println("value_gesture: " + value_gesture);
-							
-							if (value_gesture == 1 && sent == false) {
-								// send
-								gestureMap.clear();
-								gestureMap.put("type", gestureJson.getString("type"));
-								gestureMap.put("value", 1);
-								String gestureJsonString = "";
-								gestureJsonString = gestureJson.toString();
-								byte[] gestureJsonByte = gestureJsonString.getBytes();
-								System.out.println("Send gesture: " + gestureJsonString);
-								out.write(gestureJsonByte);
-								out.flush();
-								sent = true;
-							} else if (!(value_gesture == 1)) {
-								sent = false;
-							}
-						}
-						gesturebr.close();
-
-						// Gaze
-						// read file
-						filename = gazeFile;
-						BufferedReader gazebr = new BufferedReader(new FileReader(filename));
-						StringBuilder gazesb = new StringBuilder();
-						line = gazebr.readLine();
-						while (line == null) {
-							line = gazebr.readLine();
-						}
-						gazesb.append(line);
-						string = gazesb.toString();
-						JSONObject gazeJson = new JSONObject(string);
-
-						// filter: get mode
-						buffer_gaze[counter_gesture % 10] = gazeJson.getInt("value");
-						counter_gaze++;
-
-						if (counter_gaze > 9) {
-							value_gaze = mode(buffer_gaze);
-							System.out.println("value_gaze: " + value_gaze);
-
-							// send
-							if (value_gaze != value_gaze_pre) {
-								gazeMap.clear();
-								gazeMap.put("type", gestureJson.getString("type"));
-								gazeMap.put("value", value_gaze);
-								String gazeJsonString = "";
-								gazeJsonString = gazeJson.toString();
-								byte[] gazeJsonByte = gazeJsonString.getBytes();
-								System.out.println("Send gaze: " + gazeJsonString);
-								out.write(gazeJsonByte);
-								out.flush();
-								gazebr.close();
-								value_gaze_pre = value_gaze;
-							}
-						}
-
-						Thread.sleep(200);
-					}
-
-				} catch (Exception e) {
-					e.printStackTrace();
-				}
-			}
-		});
-		t.start();
-	}*/
 
 }
